174b7cb305b8325e2d44ed8176d2e30e3024f18c
silas.dias <silasdjd@gmail.com>
2021-07-12 10:33:20 -0300

    first commit

---
+++ src/main/java/com/silasdias/comicsapi/ComicsapiApplication.java
@@ -0,0 +1,13 @@
+package com.silasdias.comicsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silasdias/comicsapi/ComicsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silasdias.comicsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
